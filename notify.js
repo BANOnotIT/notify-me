5e9844a30041477f41d9c11a06e1905c367f7b88
BANO.notIT <bano.notit@yandex.ru>
2019-03-29 02:24:46 +0300

    попытаться сделать нормальные нотифаи

--- notify.js
+++ notify.js
@@ -2,55 +2,94 @@
  * Created by BANO.notIT on 28.03.19.
  */
 const bot = require('./bot')
-const URL = require('url')
-const { get } = require('token')
-const streamToStr = require('stream-to-string')
-const createError = require('http-errors')
+const { get: getChatId } = require('./token')
 
-function sendMessage (req) {
-  return streamToStr(req, 'utf-8')
-    .then(text =>
-      text
+const { parse } = require('url')
+const streamToStr = require('stream-to-string')
+const Busboy = require('busboy')
+const EventEmitter = require('events')
+
+class Sender extends EventEmitter {
+  constructor (request) {
+    super()
+    this.request = request
+    this.in_progress = 0
+    this.results = []
+
+    this.on('done', () => {
+      console.log(this.in_progress)
+      if (this.in_progress === 0) {
+        this.emit('finish', this.results)
+      }
+    })
+  }
+
+  pushFile (stream, filename) {
+    ++this.in_progress
+    bot.sendDocument(this.request.chat_id, this.request)
+      .then(() => `Document ${filename} sent!`)
+      .then(this.results.push.bind(this))
+      .then(() => {
+        --this.in_progress
+        this.emit('done')
+      })
+
+      .catch(this.emit.bind(this, 'error'))
+  }
+
+  pushMessage (message, field = '') {
+    ++this.in_progress
+    streamToStr(this.request, 'utf-8')
+      .then(text => text
         ? `**You've received** (${(new Date()).toLocaleString()}):\n${text}`
         : '**You\'ve asked to remind of something**'
-    )
-    .then(text => bot.sendMessage(req.chat_id, text))
-    .then(() => 'Notification sent!')
-}
+      )
+      .then(text => bot.sendMessage(this.request.chat_id, text))
+      .then(() => `Notification ${field} sent!`)
+      .then(this.results.push.bind(this))
+      .then(() => {
+        --this.in_progress
+        this.emit('done')
+      })
 
-function sendDocument (req) {
-  return bot.sendDocument(req.chat_id, req)
-    .then(() => 'Document sent!')
+      .catch(this.emit.bind(this, 'error'))
+  }
 }
 
 module.exports = (req, res) => {
-  let result = Promise.reject(createError(500, 'Unsupported Content-Type'))
+  let { query: { token = null } } = parse(req.url, true)
 
-  let { query: { token = null } } = new URL(req.url, true)
-
-  if (!token) {
-    result = Promise.reject(createError(401))
-  } else {
-    req.chat_id = get(token)
-  }
-
-  switch (req.headers['content-type']) {
-    case 'text/markdown; charset=UTF-8':
-      result = sendMessage(req)
-      break
-
-    case 'application/octet-stream':
-      result = sendDocument(req)
-      break
-  }
-
-  result
-    .catch(e => {
-      res.status(e.status || 500)
-      res.end(e.toString())
+  getChatId(token)
+    .then(id => {
+      req.chat_id = id
+      return req
     })
+    .then(req =>
+      new Promise((resolve, reject) => {
+        let processor = new Sender(req)
+        try {
+          let busboy = new Busboy({ headers: req.headers })
+          processor.on('finish', resolve)
+          processor.on('error', reject)
+
+          busboy.on('file', (_, file, name) => processor.pushFile(file, name))
+          busboy.on('field', (_, text) => processor.pushMessage(text))
+          busboy.on('finish', () => processor.emit.bind('done'))
+        } catch (e) {
+          return reject(e)
+        }
+      })
+    )
+
     .then(data => {
-      res.status(200)
-      res.end(data)
+      res.statusCode = 200
+      data.forEach(a => {
+        console.log(a.constructor.toString())
+      })
+      res.end(data.join('\n'))
+    })
+    .catch(e => {
+      res.statusCode = e.status || 500
+      res.end(e.stack)
     })
 }
